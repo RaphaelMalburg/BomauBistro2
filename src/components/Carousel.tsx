e8bc586beb751329c673c6946cc35db3e98e139e
unknown <raphael.malburg@gmail.com>
2023-08-19 11:50:54 +0100

    carousel done

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -11,18 +11,33 @@ export default function Carousel() {
     <div className="max-w-[1440px] justify-center items-center mx-auto  mt-20 md:pt-4 padding-x flex relative text-primary-blue-400 z-20">
       <Splide
         aria-label="Bomau Bistro"
-        className="w-2/3 h-[500px] flex  flex-col overflow-hidden justify-center items-center "
+        className="w-full h-[500px] flex  overflow-hidden justify-center items-center "
         options={{
           type: "loop",
           autoplay: true,
           interval: 3000,
           pauseOnHover: false,
           drag: "free",
+          perPage: 3,
+          breakpoints: {
+            2560: {
+              perPage: 4,
+            },
+            640: {
+              perPage: 3,
+            },
+            400: {
+              perPage: 1,
+            },
+          },
+
+          lazyLoad: "nearby",
         }}>
         {images.map((image: { src: string | StaticImport; alt: string }, index: any) => (
-          <SplideSlide key={index} className="relative ">
-            <div className="relative w-[240px] h-[300px]  ">
-              <Image src={image.src} fill alt={image.alt} className="object-cover  " />
+          // eslint-disable-next-line react/jsx-no-undef
+          <SplideSlide key={index} className="flex">
+            <div className="relative w-[280px] h-[300px] mx-1 shadow-xl">
+              <Image src={image.src} fill alt={image.alt} className="object-cover " />
             </div>
           </SplideSlide>
         ))}
