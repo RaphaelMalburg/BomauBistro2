0f5869a617f0f8743e0860196a3630cfa41acb54
unknown <raphael.malburg@gmail.com>
2023-08-21 14:22:14 +0100

    menu page done

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { BsArrowLeftShort } from "react-icons/bs";
 
 const Menu = () => {
   return (
-    <main className="relative w-full h-fit bg-white">
+    <main className="relative w-full h-fit bg-white ">
+      {" "}
+      <Link href="/" className="z-50 absolute top-2 left-5">
+        <BsArrowLeftShort size={40} />
+      </Link>
       <Image src={"/menu1.png"} width={500} height={1000} alt="menu" className=" object-contain" />
-
       <Image src={"/menu2.png"} width={500} height={1000} alt="menu" className=" object-contain" />
     </main>
   );
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -36,7 +36,7 @@ const Hero = () => {
   };
 
   return (
-    <section className="flex lg:flex-row flex-col-reverse gap-5 relative z-0 max-w-[1440px] justify-center items-center mx-auto   text-primary-blue-100 h-fit mb-40 md:mb:0">
+    <section className="flex lg:flex-row flex-col-reverse gap-5 relative z-0 max-w-[1440px] justify-center items-center mx-auto   text-primary-blue-100 h-fit mb-52 md:mb:0">
       <div className="flex-[1.5] padding-x grid gap-4">
         <motion.div
           variants={container}
@@ -71,7 +71,7 @@ const Hero = () => {
         <div className=" absolute bg-gradient-to-r from-primary-blue-285 via-primary-blue-300 to-primary-blue-400 rounded-full mx-4 md:mx:10 left-0 -bottom-12 py-2 md:w-full w-11/12   " />
       </div>
       <div className="xl:flex-1 flex justify-end items-end md:w-4/5 w-full lg:w-[480px] xl:h-fit md:mt-60 ">
-        <div className="relative xl:w-[570px] mx-2 xs1:w-full  xl:h-[450px] xs1:mt-60 w-[350px]  justify-center mt-48 md:-mt-10 my-auto h-[320px] z-0 ">
+        <div className="relative xl:w-[570px] mx-2 xs1:w-full  xl:h-[450px] xs1:mt-60 w-[350px]  justify-center mt-56 md:-mt-10 my-auto h-[320px] z-0 ">
           <Image
             src="/salad.png"
             alt="hero"
